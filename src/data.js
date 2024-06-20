fd0764879af467d2ddbd154f647441a8188cc58a
Lucky Perez <luckyperez321@gmail.com>
2024-06-20 08:26:51 -0500

    add a new module and export name array

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const names =[
+    { person: "Lucky", id: "001" }
+    { person: "Dakota", id: "002" }
+    { person: "Kloe", id: "003" }
+    { person: "Waffles", id: "004" }
+    { person: "Rex", id: "005" }
+    { person: "Mocha", id: "006" }
+
+
+];
