fe570e490a5e25101411aca7c8df009a92b2795a
Lucky Perez <luckyperez321@gmail.com>
2024-07-31 09:23:25 -0500

    change functional component to staeful

--- src/data.js
+++
@@ -1,9 +0,0 @@
-export const names =[
-    { person: "Lucky", id: "001" },
-    { person: "Dakota", id: "002" },
-    { person: "Kloe", id: "003" },
-    { person: "Waffles", id: "004" },
-    { person: "Rex", id: "005" },
-    { person: "Mocha", id: "006" }
-];
-
--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
-import {names} from "./data.js";
+
 
 import "./styles.css";
 
 const renderNameTag = (name) => 
-  <NameTag name = {name.person} key = {name.id} />
+  <NameTag name = {name} key = {name} />
 
-const App = () => {
-  const NameTagElements = names.map(renderNameTag);
+class App extends Component {
+
+  state = {names: ["Lucky", "Dakota", "Kloe", "Waffles", "Rex", "Mocha"]}
+  render(){
+  const NameTagElements = this.state.names.map(renderNameTag);
+  
   return (
   <div className="App">
     <h1>Name Tag Generator</h1>
     {NameTagElements}
   </div>
   );
+}
 };
 
 const rootElement = document.getElementById("root");
