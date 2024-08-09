ce28d9edc91a44ff7212c0cba890f18fc618fae7
Lucky Perez <luckyperez321@gmail.com>
2024-08-09 13:37:10 -0500

    added some lifecycle methods to COMPLETE project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import UserInput from "./UserInput.js";
 
 class App extends Component {
   state = {
-    names: ["Dakota", "Kloe", "Waffles", "Lucky", "Mocha"],
+    names: [],
   };
   removeName = (clickedIndex) => {
     // to learn how the .filter method works, check out https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
@@ -17,6 +17,19 @@ class App extends Component {
     const newNames = [name, ...this.state.names];
     this.setState({ names: newNames });
   };
+
+  componentDidUpdate() {
+    const savedNameString = JSON.stringify(this.state.names);
+    localStorage.setItem("savedNames", savedNameString);
+  }
+
+  componentDidMount() {
+    const savedNamesString = localStorage.getItem("savedNames");
+    if (savedNamesString) {
+      const savedNames = JSON.parse(savedNamesString);
+      this.setState({ names: savedNames });
+    }
+  }
 
   render() {
     return (
